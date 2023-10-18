b21ba0d381883d109b3de26fecdb43b9679b6f35
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2023-10-18 22:12:54 +0900

    백준허브 연동, 주석제거

--- SWEA/D1/2072. 홀수만 더하기.py
+++
@@ -1,9 +0,0 @@
-T = int(input())
-
-for i in range(T):
-    arr = list(map(int, input().split()))
-    sum = 0
-    for j in range(len(arr)):
-        if arr[j] % 2 != 0:
-           sum += arr[j]
-    print(f'#{i+1} {sum}')
