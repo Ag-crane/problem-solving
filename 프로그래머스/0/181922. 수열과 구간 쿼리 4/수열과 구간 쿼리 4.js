f0b904157ab3f94af9c004710d66c03f1f71f288
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-01-26 13:08:56 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.15 ms, Memory: 36 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+    var answer = [];
+    queries.map(([s,e,k]) => {
+        for (let i=s;i<=e;i++){
+            if (i%k===0){
+                arr[i]++
+            }
+        }
+    })
+    return arr;
+}
