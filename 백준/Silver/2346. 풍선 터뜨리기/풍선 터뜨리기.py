e6bcc0152acaf17ee1ca7b741d69fae937de1c45
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-02-20 19:57:46 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,14 +1,23 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-n = int(input())  # 풍선의 개수 입력
-data = list(map(int, input().split()))  # 각 풍선 안의 숫자 입력
+n = int(input())
+data = list(map(int,input().split()))
 
-dq = deque([i for i in range(1, n+1)])  # 풍선의 초기 순서를 데크로 생성
+dq = deque([i for i in range(1,n+1)])
 
-while dq:
-    target = dq.popleft()  # 현재 풍선을 터뜨림
-    print(target, end=' ')  # 터뜨린 풍선의 번호 출력
+answer = []
 
-    if dq:  # 아직 터뜨릴 풍선이 남아있다면
-        move = data[target-1]  # 터뜨린 풍선 안의 숫자로 이동할 거리 결정
-        dq.rotate(-move if move < 0 else -(move-1))  # 풍선 이동(음수일 경우 그대로, 양수일 경우 1 감소)
+while len(dq)!=0:
+    target = dq[0] # 1
+    move = data[target-1] 
+    answer.append(str(dq.popleft()))
+    if move > 0:
+        dq.rotate(-(move-1))
+    else:
+        dq.rotate(-move)
+#     print(dq)
+
+# print(answer)
+print(' '.join(answer))
