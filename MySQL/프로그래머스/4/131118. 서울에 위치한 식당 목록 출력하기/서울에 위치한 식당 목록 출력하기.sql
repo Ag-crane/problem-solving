3f9e3b0abbd15c85dff0a63fd186ffe1b1b6992e
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-03-02 11:36:46 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ MySQL/프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,22 +1,12 @@
-SELECT 
-    RI.REST_ID, 
-    RI.REST_NAME, 
-    RI.FOOD_TYPE, 
-    RI.FAVORITES, 
-    RI.ADDRESS, 
-    ROUND(AVG(RR.REVIEW_SCORE), 2) AS AVG_REVIEW_SCORE
-FROM 
-    REST_INFO RI
-JOIN 
-    REST_REVIEW RR ON RI.REST_ID = RR.REST_ID
-WHERE 
-    RI.ADDRESS LIKE '서울%'
-GROUP BY 
-    RI.REST_ID, 
-    RI.REST_NAME, 
-    RI.FOOD_TYPE, 
-    RI.FAVORITES, 
-    RI.ADDRESS
-ORDER BY 
-    AVG_REVIEW_SCORE DESC, 
-    RI.FAVORITES DESC;
+# 서울에 위치한 식당들의
+# 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수
+# 리뷰 평균점수는 소수점 세 번째 자리에서 반올림
+# 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬
+
+select i.rest_id, i.rest_name, i.food_type, i.favorites, i.address, 
+    round(avg(r.review_score),2) as score
+from rest_info i
+join rest_review r on i.rest_id = r.rest_id
+where i.address like '서울%'
+group by i.rest_id
+order by score desc, i.favorites desc
