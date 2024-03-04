f595c199385fdad00eee738b1f444f477ce7dbc8
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-03-05 00:07:45 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/42862. 체육복/README.md
+++ Python3/프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 23:58:38
+2024년 03월 05일 00:07:42
 
 ### 문제 설명
 
--- Python3/프로그래머스/1/42862. 체육복/체육복.py
+++ Python3/프로그래머스/1/42862. 체육복/체육복.py
@@ -1,16 +1,24 @@
 def solution(n, lost, reserve):
-    # 실제 여벌 체육복을 가져온 학생 중에서 도난 당하지 않은 학생 찾기
-    real_reserve = set(reserve) - set(lost)
-    # 도난 당했지만 여벌 체육복이 없는 학생 찾기
-    real_lost = set(lost) - set(reserve)
+    answer = n - len(lost)
+    lost.sort()
+    reserve.sort()
+    jungbok = []
+    for num in lost:
+        if num in reserve:
+            jungbok.append(num)
+    for num in jungbok:
+        lost.remove(num)
+        reserve.remove(num)
+        answer+=1
+            
+    if len(lost)==0 or len(reserve)==0:
+        return answer
     
-    for r in real_reserve:
-        # 여벌 체육복이 있는 학생의 앞번호 학생이 체육복을 잃어버렸는지 확인
-        if r - 1 in real_lost:
-            real_lost.remove(r - 1)
-        # 여벌 체육복이 있는 학생의 뒷번호 학생이 체육복을 잃어버렸는지 확인
-        elif r + 1 in real_lost:
-            real_lost.remove(r + 1)
-    
-    # 체육수업을 들을 수 있는 학생의 수는 전체 학생 수에서 여벌 체육복을 받지 못한 학생 수를 뺀 것
-    return n - len(real_lost)
+    for num in lost :
+        if num-1 in reserve:
+            reserve.remove(num-1)
+            answer += 1
+        elif num+1 in reserve:
+            reserve.remove(num+1)
+            answer += 1
+    return answer
