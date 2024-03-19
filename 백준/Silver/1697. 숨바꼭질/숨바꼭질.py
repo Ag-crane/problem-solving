236832daf279f0907ca9899a84cc8a13fb1c4017
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-03-19 15:27:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35656 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -4,7 +4,7 @@ from collections import deque
 input = sys.stdin.readline
 
 n, k = map(int,input().split())
-graph = [0] * 100001
+arr = [0] * 100001
 
 def bfs(x):   
     q = deque([x])
@@ -12,14 +12,13 @@ def bfs(x):
         return 0
     while q:
         x = q.popleft()
-        
+        if x == k:
+            return arr[x]
         for i in [x-1,x+1,2*x]:
             if i<0 or i>100000:
                 continue
-            if i==k:
-                return(graph[x]+1)
-            if graph[i]==0:
-                graph[i] = graph[x] + 1
+            if arr[i]==0:
+                arr[i] = arr[x] + 1
                 q.append(i)
 
 
