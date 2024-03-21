93991fc5091284b43dca51fa31aa650ba06c6fe3
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-03-21 10:47:52 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 152 ms, Memory: 36088 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+n, k = map(int,input().split())
+
+def bfs(x):
+    start = x
+    prev_pos = [-2]*100001
+    q = deque([x])
+    prev_pos[x] = -1
+    
+    while q:
+        # print(q)
+        x = q.popleft()
+        
+        if x==k:
+            trail = [x]
+            prev = prev_pos[x]
+            while prev != -1:
+                trail.append(prev)
+                prev = prev_pos[prev]
+            
+            return trail
+
+        for nx in [x-1,x+1,2*x]:
+            if nx<0 or nx>100000:
+                continue
+            if prev_pos[nx]==-2 :
+               q.append(nx)
+               prev_pos[nx] = x
+
+    return trail
+
+result = bfs(n)
+print(len(result)-1)
+for i in reversed(result):
+    print(i,end=' ')
