434a07c6096369e502528568dc73f31da8a66140
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-03-23 08:42:00 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.47 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,17 +1,13 @@
-function find(arr, [s,e,k]) {
-    let lst=[];
-    for (let i=s;i<=e;i++){
-        if (arr[i]>k){
-            lst.push(arr[i])
-        }
-    }
-    return lst.length===0 ? -1 : Math.min(...lst)
-}
-
 function solution(arr, queries) {
     var answer = [];
-    queries.forEach((query)=>{
-        answer.push( find(arr,query) )
+    queries.forEach(([s,e,k])=>{
+        let lst=[];
+        for (let i=s;i<=e;i++){
+            if (arr[i]>k){
+                lst.push(arr[i])
+            }
+        }
+        answer.push( lst.length===0 ? -1 : Math.min(...lst) )
     })
     
     return answer;
