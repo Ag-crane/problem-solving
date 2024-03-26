a4eb66626b6c685cc7fe07db2d9571372f341c43
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-03-26 09:39:57 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,27 +1,26 @@
 def solution(N, stages):
-    stage_reached = [0] * (N + 1)  # 각 스테이지에 도달한 플레이어 수 (인덱스 0은 사용하지 않음)
-    stage_not_cleared = [0] * (N + 1)  # 각 스테이지에서 멈춰있는 플레이어 수 (인덱스 0은 사용하지 않음)
-
-    # 각 스테이지에 도달한 플레이어 수와 멈춰있는 플레이어 수 계산
-    for stage in stages:
-        for i in range(1, stage):
-            stage_reached[i] += 1
-        if stage <= N:  # N+1 스테이지는 모든 스테이지를 클리어한 것이므로 제외
-            stage_reached[stage] += 1
-            stage_not_cleared[stage] += 1
-
-    # 실패율 계산 및 (실패율, 스테이지 번호) 튜플 리스트 생성
-    fail_rates = []
-    for i in range(1, N + 1):
-        if stage_reached[i] == 0:  # 스테이지에 도달한 플레이어가 없는 경우
-            fail_rate = 0
+    answer = []
+    # 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
+    # playing / all_reached
+    playing = [0]*(N+1)
+    all_reached = [0]*(N+1)
+    for i in range(len(stages)):
+        if stages[i]==N+1 :
+            for j in range(1,N+1):
+                all_reached[j] += 1
         else:
-            fail_rate = stage_not_cleared[i] / stage_reached[i]
-        fail_rates.append((fail_rate, i))
-
-    # 실패율에 따라 정렬 (실패율이 같을 경우 스테이지 번호가 작은 순으로)
-    fail_rates.sort(key=lambda x: (-x[0], x[1]))
-
-    # 정렬된 스테이지 번호 추출
-    answer = [stage for _, stage in fail_rates]
+            playing[stages[i]] += 1
+            for j in range(1,stages[i]+1):
+                all_reached[j] += 1
+    failure_lst = []
+    for i in range(1,N+1):
+        if all_reached[i]==0:
+            failure_lst.append((0,i))
+        else:
+            failure_lst.append((playing[i] / all_reached[i],i))
+    failure_lst.sort(key=lambda x:(-x[0],x[1]))
+    
+    for i in range(N):
+        answer.append(failure_lst[i][1])
+    
     return answer
