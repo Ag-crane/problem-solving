48c7356ffa34e519c65d3efd14b462976f41c4b0
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-05-22 11:18:41 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,16 +1,19 @@
-select id
-from ecoli_data
-where parent_id in (
-    select id
-    from ecoli_data
-    where parent_id in (
-        select id
-        from ecoli_data
-        where parent_id is null
-    )
-)
-order by id
+# select id
+# from ecoli_data
+# where parent_id in (
+#     select id
+#     from ecoli_data
+#     where parent_id in (
+#         select id
+#         from ecoli_data
+#         where parent_id is null
+#     )
+# )
+# order by id
 
-
-
-
+select c.id
+from ecoli_data a
+    join ecoli_data b on a.id = b.parent_id
+    join ecoli_data c on b.id = c.parent_id
+where a.parent_id is null
+order by c.id
