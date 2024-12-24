6b98d6c87ae1b2118d0eeda3e13d0c6c3ef97a57
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-12-24 12:54:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 46.49 ms, Memory: 37.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ JavaScript/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,14 +1,15 @@
 function solution(bridge_length, weight, truck_weights) {
     const trucks = truck_weights.reverse() // 스택
     const bridge = Array(bridge_length).fill(0) // 큐
-    
+    let bridge_weight = 0
     let t = 0
-    while (bridge.some(e=>e!==0) || trucks.length>0) {
+    while (bridge_weight>0 || trucks.length>0) {
         t++
-        bridge.shift()
-        const sum = bridge.reduce((a,c) => a+c)
-        if (trucks.at(-1) + sum <= weight) {
-            bridge.push(trucks.pop())
+        bridge_weight -= bridge.shift()
+        if (trucks.at(-1) + bridge_weight <= weight) {
+            let next_truck = trucks.pop()
+            bridge.push(next_truck)
+            bridge_weight += next_truck
         } else {
             bridge.push(0)
         }
