dbf45bbec6a9bfb783a9029dfd5af0ae5dc3f4da
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2025-01-08 17:13:30 +0900

    [Silver III] Title: N과 M (5), Time: 224 ms, Memory: 31288 KB -BaekjoonHub

---
+++ node.js/백준/Silver/15654. N과 M （5）/N과 M （5）.js
@@ -0,0 +1,24 @@
+const input = require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString().trim().split(/\r?\n/)
+const [N, M] = input[0].split(' ').map(Number)
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b)
+
+const result = []
+
+function backtrack(cur, depth) {
+    
+    if (depth===M) {
+    result.push([...cur])
+        return
+    }
+
+    for (let i=0; i<N; i++){
+        if (cur.includes(arr[i])) continue
+        cur.push(arr[i])
+        backtrack(cur, depth+1)
+        cur.pop()
+    }
+}
+
+backtrack([], 0)
+
+console.log(result.map(arr => arr.join(' ')).join('\n'))
