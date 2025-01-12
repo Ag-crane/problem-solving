9486d975b46b5456058ed35de52c1e658c0d8636
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2025-01-12 23:59:03 +0900

    [Gold IV] Title: N-Queen, Time: 3348 ms, Memory: 10620 KB -BaekjoonHub

--- node.js/백준/Gold/9663. N－Queen/N－Queen.js
+++ node.js/백준/Gold/9663. N－Queen/N－Queen.js
@@ -1,9 +1,8 @@
 const N = +require('fs').readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt').toString()
 
-const board = Array(N).fill(-1) // 1차원 배열로 저장. index가 row, 값이 col
 const diagonalL = Array(N*2).fill(false)
 const diagonalR = Array(N*2).fill(false)
-
+const isColOccupied = Array(N).fill(false)
 let result = 0
 
 function backtrack(row) {
@@ -13,19 +12,17 @@ function backtrack(row) {
     }
 
     for (let col=0; col<N; col++) {
-        if (board[row] !== -1) continue // 행 체크
-        if (board.some(x => x===col)) continue // 열 체크
-        if (diagonalL[row - col + N] || diagonalR[row + col]) continue
-        board[row] = col
+        if (isColOccupied[col]) continue // 열 체크
+        if (diagonalL[row - col + N] || diagonalR[row + col]) continue // 대각선 체크
+        isColOccupied[col] = true
         diagonalL[row - col + N] = true
         diagonalR[row + col] = true
         backtrack(row + 1)
-        board[row] = -1
+        isColOccupied[col] = false
         diagonalL[row - col + N] = false
         diagonalR[row + col] = false
     }
 }
 
 backtrack(0)
-
 console.log(result)
