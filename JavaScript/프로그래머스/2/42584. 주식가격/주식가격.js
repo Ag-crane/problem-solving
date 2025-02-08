49c8838fae5e48cb40ec470151dca12cbf7dc826
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2025-02-08 16:09:43 +0900

    [level 2] Title: 주식가격, Time: 14.39 ms, Memory: 44.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/42584. 주식가격/주식가격.js
+++ JavaScript/프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,13 +1,20 @@
 function solution(prices) {
-    var answer = Array(prices.length).fill(0);
-    
-    for (let i=0; i<prices.length; i++) {
-        for (let j=i+1; j<prices.length; j++) {
-            answer[i]++
-            if (prices[i] > prices[j]) {
-                break
-            } 
+    const results = Array(prices.length).fill(0)
+    const stack = []
+    stack.push(0)
+    for (let i=1; i<prices.length; i++) {
+        let time = 1
+        while (stack.length > 0 && prices[stack.at(-1)] > prices[i]) {
+            const popIndex = stack.pop()
+            results[popIndex] = i - popIndex
         }
+        stack.push(i)
     }
-    return answer;
+    // console.log(results)
+    while (stack.length>0) {
+        const popIndex = stack.pop()
+        results[popIndex] = prices.length - popIndex - 1
+    }
+    
+    return results
 }
