2658ee7ae29818d3f2c86df2b44248e2c8cecde6
Eunhak Lee <eunhak0401@naver.com>
2025-02-20 09:40:56 +0900

    [level 2] Title: 할인 행사, Time: 39.40 ms, Memory: 40.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/131127. 할인 행사/README.md
+++ JavaScript/프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 42.9 MB, 시간: 42.95 ms
+메모리: 40.8 MB, 시간: 39.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 19일 18:07:12
+2025년 02월 20일 09:40:54
 
 ### 문제 설명
 
--- JavaScript/프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ JavaScript/프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,24 +1,33 @@
-
-
 function solution(want, number, discount) {
     let result = 0;
     
-    let wantTable = {}
+    let needed = {}
     for (let i=0; i<want.length; i++) {
-        wantTable[want[i]] = number[i]
+        needed[want[i]] = number[i]
     }
     
-    function check(products, wantTable) {
-        for (const product of products) {
-            if (wantTable[product]===undefined) return false
-            else wantTable[product]--
+    let currentCount = {} // i: 0~9
+    for (let i=0; i<10; i++) {
+        currentCount[discount[i]] = (currentCount[discount[i]] || 0) + 1
+    }
+
+    function check() {
+        for (const key of Object.keys(needed)) {
+            if (needed[key]!==currentCount[key]) {
+                return false
+            }
         }
-        
-        return Object.values(wantTable).every(value => value<=0) ? true : false
+        return true
     }
     
-    for (let i=0; i<=discount.length - 10; i++) {
-        if (check(discount.slice(i, i+10), {...wantTable})) result++
+    if (check()) result++
+    
+    // 슬라이딩 윈도우 적용
+    for (let i=0; i<discount.length - 10; i++) {
+        currentCount[discount[i]]-- // 맨 앞 제거
+        currentCount[discount[i+10]] = (currentCount[discount[i+10]] || 0) + 1 // 맨 뒤 추가
+        
+        if (check()) result++
     }
     
     return result;
