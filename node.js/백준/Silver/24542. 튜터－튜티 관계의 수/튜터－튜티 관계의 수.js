7aca93e6132fd670dc74c670f8ce9f8dbd3ad1b4
Eunhak Lee <eunhak0401@naver.com>
2025-03-01 18:21:27 +0900

    [Silver I] Title: 튜터-튜티 관계의 수, Time: 500 ms, Memory: 71852 KB -BaekjoonHub

--- node.js/백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.js
+++ node.js/백준/Silver/24542. 튜터－튜티 관계의 수/튜터－튜티 관계의 수.js
@@ -5,41 +5,51 @@ const input = require("fs")
     .split(/\r?\n/);
 
 const [N, M] = input[0].split(" ").map(Number);
+const MOD = 1000000007;
 
-const graph = Array.from({ length: N + 1 }, () => []);
+// 유니온-파인드 풀이
+const parent = Array(N + 1)
+    .fill(0)
+    .map((_, i) => i);
+
+const rank = Array(N + 1).fill(0);
+
+function find(x) {
+    if (parent[x] === x) {
+        return x;
+    } else {
+        parent[x] = find(parent[x]);
+        return parent[x];
+    }
+}
+
+function union(x, y) {
+    const rootX = find(x);
+    const rootY = find(y);
+
+    if (rootX === rootY) return;
+
+    if (rank[rootX] > rank[rootY]) {
+        parent[rootY] = rootX;
+    } else if (rank[rootX] < rank[rootY]) {
+        parent[rootX] = rootY;
+    } else {
+        parent[rootX] = rootY;
+        rank[rootY]++;
+    }
+}
 
 for (let i = 1; i <= M; i++) {
     const [a, b] = input[i].split(" ").map(Number);
-    graph[a].push(b);
-    graph[b].push(a);
+    union(a, b);
 }
 
-const visited = Array(N + 1).fill(false);
-
-function countNode(start) {
-    let count = 0;
-
-    function dfs(current) {
-        if (visited[current]) return;
-
-        visited[current] = true;
-        count++;
-
-        for (const next of graph[current]) {
-            if (!visited[next]) {
-                dfs(next);
-            }
-        }
-    }
-
-    dfs(start);
-
-    return count;
-}
-
-let result = 1;
+const countRoot = {};
 for (let i = 1; i <= N; i++) {
-    if (!visited[i]) result = (result * countNode(i)) % 1000000007;
+    const root = find(i);
+    countRoot[root] = (countRoot[root] || 0) + 1;
 }
+
+const result = Object.values(countRoot).reduce((a, c) => (a * c) % MOD);
 
 console.log(result);
