361fe6ed241b4520e05477674d5927e982d4bb7a
Eunhak Lee <eunhak0401@naver.com>
2025-03-02 23:12:13 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ MySQL/프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,23 +1,14 @@
-SELECT C.ITEM_ID, C.ITEM_NAME, C.RARITY
-FROM ITEM_INFO C
-    JOIN (
-        SELECT A.ITEM_ID AS 'CHILD_ID', B.ITEM_ID AS 'RARE_ID'
-        FROM ITEM_TREE A
-            JOIN (
-                SELECT ITEM_ID
-                FROM ITEM_INFO
-                WHERE RARITY = 'RARE'
-            ) B
-            ON A.PARENT_ITEM_ID = B.ITEM_ID
-    ) D
-    ON C.ITEM_ID = D.CHILD_ID
-ORDER BY C.ITEM_ID DESC
-
-
-
-
-# ITEM_ID	RARITY
-# 0	        RARE
-# 1	        RARE
-# 3	        RARE
-# 4	        RARE
+select c.item_id, c.item_name, c.rarity
+from item_info c
+join (
+    select b.item_id
+    from (
+        select item_id
+        from item_info
+        where rarity = 'RARE'
+        ) a
+    join item_tree b
+    on a.item_id = b.parent_item_id
+) d
+on c.item_id = d.item_id
+order by c.item_id desc
