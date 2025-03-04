b2c76c124ae1608feec1a9343536a0f38b3ebc9a
Eunhak Lee <eunhak0401@naver.com>
2025-03-04 10:00:35 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ MySQL/프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,14 +1,22 @@
+with grouped as (
+    select id, group_concat(distinct category) as categories, group_concat(name) as skills
+    from developers d
+    join skillcodes s
+    where skill_code & code > 0
+    group by id
+)
+
 select 
     case
-        when group_concat(s.name) like '%Python%' and
-            group_concat(s.category) like '%Front End%' then 'A'
-        when group_concat(s.name) like '%C#%' then 'B'
-        when group_concat(s.category) like '%Front End%' then 'C'
+        when find_in_set('Front End', categories) > 0 and find_in_set('Python', skills) then 'A'
+        when find_in_set('C#', skills) > 0 then 'B'
+        when find_in_set('Front End', categories) > 0 then 'C'
     end as grade,
-    d.id,
-    d.email
-from developers d
-join skillcodes s on d.skill_code & s.code <> 0
-group by d.id, d.email
-having grade is not null
-order by grade, d.id
+    id,
+    email
+from grouped g
+join developers d
+using (id)
+where find_in_set('Front End', categories) > 0 or find_in_set('C#', skills) > 0
+order by grade, id
+
