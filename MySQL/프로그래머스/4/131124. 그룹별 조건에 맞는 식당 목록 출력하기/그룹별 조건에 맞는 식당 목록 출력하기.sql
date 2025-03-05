1f0d7092122004e15d9dc851ca94320e037477f4
Eunhak Lee <eunhak0401@naver.com>
2025-03-06 00:25:59 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ MySQL/프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,20 +1,13 @@
-with best_reviewers as (
-    select member_id, count(*) as count
+with ranked_cte as (
+    select member_id, count(*) as 'count', rank() over (order by count(*) desc) as 'rank'
     from rest_review
     group by member_id
-    having count = (
-                select count(*) as count
-                from rest_review
-                group by member_id
-                order by count desc
-                limit 1
-            )
+    order by count desc
 )
 
-select p.member_name, r.review_text, date_format(r.review_date, '%Y-%m-%d') as review_date
-from member_profile p 
-join rest_review r on p.member_id = r.member_id
-join best_reviewers b on r.member_id = b.member_id
-order by r.review_date, r.review_text
-
-
+select a.MEMBER_NAME, b.REVIEW_TEXT, date_format(b.REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
+from member_profile a
+join rest_review b on a.member_id = b.member_id
+join ranked_cte c on a.member_id = c.member_id
+where c.rank = 1
+order by REVIEW_DATE, REVIEW_TEXT
