91dd9dbe77ad76c4f7f90ba9cd94268bccb3c7ed
Eunhak Lee <eunhak0401@naver.com>
2025-03-18 10:45:20 +0900

    [level 3] Title: 정수 삼각형, Time: 10.79 ms, Memory: 41.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ JavaScript/프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,13 +1,13 @@
 function solution(triangle) {
-    const dp = triangle.map(row => row.map(num => 0))
-    dp[0] = triangle[0]
+    const dp = [triangle[0]]
     
     for (let i=1; i<triangle.length; i++) {
-        for (let j=0; j<triangle[i].length; j++) {
-            dp[i][j] = Math.max(
+        dp.push([])
+        for (let j=0; j<=i; j++) {
+            dp[i].push(Math.max(
                 (dp[i-1][j] || 0) + triangle[i][j], 
                 (dp[i-1][j-1] || 0) + triangle[i][j]
-            )
+            ))
         }
     }
     // console.log(dp)
